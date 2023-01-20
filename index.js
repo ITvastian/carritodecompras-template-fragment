94415c0d547a4cf8163b40f372e2764d438b48ff
ITvastian <sebiquesada11@gmail.com>
2023-01-20 20:06:38 -0300

    carrito de compras

---
+++ index.js
@@ -0,0 +1,32 @@
+// const nombre= "sebas";
+
+// const gato= {
+//     nombre: "valiiiiente",
+//     duerme: true,
+//     edad: 10,
+//     enemigos: ["agua", "perro"],
+    // comer: (alimento)  => {
+    //    document.write(
+    //         `${gato.nombre} esta comiendo ${alimento}`
+    //         );
+    // },
+
+    // listarEnemigos(){
+    //     this.enemigos.forEach((item) =>document.write(item))
+    // },
+    
+// };
+// destructurar , alias. crea un numero nombre para la variable nombre
+// const {nombre : nombreSuperGato = "sin nombre"} = gato
+// document.write(nombreSuperGato  )
+// ----------------------------------------
+// gato.comer("pez");
+// gato.listarEnemigos();
+//  for (let propiedad in gato){
+//     document.write(gato[propiedad])
+//  }
+
+// document.write(Object.values(gato))
+// Object.values(gato).forEach((item) =>console.log(item));
+
+
---
+++ readme.md
@@ -0,0 +1,27 @@
+#ejemplo de readme
+
+##ejemplo de sumtitulo
+
+###super ejemplo
+####mas ejemplo. se ve que cada vez va mas pequeño
+
+#####siii. por lo visto si
+
+```
+htmnl
+aqui va el codigo de HTML
+
+
+<template id="template">
+    <li class="list-group-item d-flex justify-content-between align-items-center">
+        <span class="lead">frutilla</span>
+        <span class="badge bg-primary rounded-pill">12</span>
+    </li>
+</template>
+
+
+```
+
+
+#this is great! the proyecto is created by ITVastian.
+#Do you see anything that needs to be changed?
